83282766a9415ef25e09e5f213a0aab41f6c2ac9
Ajar <aja_sarmad@hotmail.com>
2023-04-01 03:56:12 +0500

    Added Registrations controllers

--- Backend/src/main/java/com/univent/controllers/EventController.java
+++ Backend/src/main/java/com/univent/controllers/EventController.java
@@ -1,11 +1,9 @@
 package com.univent.controllers;
 
-import java.io.IOException;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,16 +11,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.univent.models.Event;
 import com.univent.models.EventViewModel;
 import com.univent.models.Union;
 import com.univent.repositories.EventRepository;
 import com.univent.repositories.UnionRepository;
-import com.univent.serviceImpl.ImageUtil;
 
 @RestController
 @RequestMapping("/api/event")
@@ -32,11 +27,14 @@ public class EventController {
 	@Autowired
 	UnionRepository unionRepository;
 	
+	
+	
+	/* ADD IMAGES KA FEATURE DALNA HAI BAAD MAI*/
 	//Add Event Method
 	//http://localhost:8080/api/event/addEvent
 	@CrossOrigin(origins = "http://localhost:8081")
 	@PostMapping("/addEvent")
-	public ResponseEntity<Object> addEvent(@RequestPart("file") MultipartFile file, @RequestBody EventViewModel eventViewModel ) throws IOException{
+	public ResponseEntity<Object> addEvent(@RequestBody EventViewModel eventViewModel ){
 	
 		try {
 			Optional<Union> union = unionRepository.findByName(eventViewModel.getUnionName());
@@ -55,7 +53,6 @@ public class EventController {
 						null,
 						union.get(),
 						null);
-				newEvent.setEventImage(ImageUtil.compressImage(file.getBytes()));
 				if(event.isEmpty())
 					return new ResponseEntity<Object>(eventRepository.save(newEvent),HttpStatus.CREATED);
 				else {
@@ -69,6 +66,21 @@ public class EventController {
 		}
 	}
 	
+	
+	//Get ALL EVENTS
+    //http://localhost:8080/api/event/getAllEvents
+    @CrossOrigin(origins = "http://localhost:8081")
+	@GetMapping("/getAllEvents")
+    public ResponseEntity<Object> getAllEvents(){
+    	try {
+    	return new ResponseEntity<Object>(eventRepository.findAll(), HttpStatus.OK);
+    	}
+    	catch(Exception ex) {
+    		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex);
+    	}
+    }
+    
+    
 	//Get Event Profile
     //http://localhost:8080/api/event/getEvent?name=ACM
     @CrossOrigin(origins = "http://localhost:8081")
@@ -83,21 +95,6 @@ public class EventController {
     	}
     }
 
-
-    //GET Event IMAGE
-  	//http://localhost:8080/api/event/getImage?id=1440
-    @CrossOrigin(origins = "http://localhost:8081")
-  	@GetMapping("/getImage")
-    public ResponseEntity<Object> getImage(@RequestParam(name="name") String name){
-  		
-    	try {
-    	Optional<Event> event = eventRepository.findByName(name);
-    		byte[] image = ImageUtil.decompressImage(event.get().getEventImage());
-    		return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.valueOf("image/jpeg")).body(image);
-    	}catch(Exception ex) {
-  			return new ResponseEntity<Object>(HttpStatus.NOT_FOUND);
-    	}
-  	}
 
 	
 }
